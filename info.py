326993d5245e19e431efe429a14dc278ea193936
HG BOTS <92435453+hemanth233739@users.noreply.github.com>
2025-06-29 13:42:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,13 +84,13 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
-PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
+PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'filestore302') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hgbotsupportgroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/tamilchatting24x7')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hgbotupdates')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'hgbotsupportgroup') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'tamilchatting24x7') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/tgmaster_pro')
 
 # True Or False
